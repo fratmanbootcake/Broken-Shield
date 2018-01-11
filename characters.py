add3830aa18722c6773dbe7ad3f794e1aef93c8f
fratmanbootcake <35295328+fratmanbootcake@users.noreply.github.com>
2018-01-11 18:11:24 +0000

    Update characters.py
    Removed some obsolete code.

--- characters.py
+++ characters.py
@@ -18,17 +18,4 @@ class Character:
         if self.health > 0:
             return True
 
-##    def attack(self, other):
-##        roll = random.randint(1, 20)
-##        if roll + self.attack_bonus > 10 + other.armour.defence:
-##            damage = Die(self.weapon.damage).roll()
-##            other.health -= damage
-##            print("{} hit {} for {} damage!".format(self.name, other.name, damage))
-##        else:
-##            print("{} missed!".format(self.name))
-##        if not other.is_alive():
-##            print("{} has slain {}!".format(self.name, other.name))
-##            return True
 
-##    def dodge(self):
-##        pass
